ba605a7d082a09000584cbd549bc45f64ecbdcbd
Honghyesoo <ghdgptn12@naver.com>
2024-12-06 21:48:55 +0900

    매니저 대기번호목록구현

--- src/main/java/zerobase/tableNow/domain/store/controller/manager/service/impl/ManagerServiceImpl.java
+++ src/main/java/zerobase/tableNow/domain/store/controller/manager/service/impl/ManagerServiceImpl.java
@@ -53,23 +53,31 @@ public class ManagerServiceImpl implements ManagerService {
     }
 
 
-    /**
-     * 매니저 전용 예약 정보 확인
-     * @param store
-     * @return 본인 상점에 대한 예약정보
-     */
-    @Override
-    public List<ConfirmDto> confirmList(String store) {
-        List<ReservationEntity> reservations = managerRepository.findByStore_Store(store);
+        /**
+         * 매니저 전용 예약 정보 확인
+         * @param user
+         * @return 본인 상점에 대한 예약정보
+         */
+        @Override
+        public List<ConfirmDto> confirmList(String user) {
+            UsersEntity userId = userRepository.findByUser(user)
+                    .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
 
-        // 예약 날짜와 시간을 기준으로 오름차순 정렬
-        return reservations.stream()
-                .map(reservation -> ConfirmDto.builder()
-                        .store(reservation.getStore().getStore())
-                        .phone(reservation.getPhone())
-                        .peopleNb(reservation.getPeopleNb())
-                        .build())
-                .collect(Collectors.toList());
-    }
+            // 사용자에 대한 예약 정보를 조회
+            List<ReservationEntity> reservations = managerRepository.findByUser(userId);
 
+            // 대기번호가 부여된 예약만 필터링하고, 대기번호를 기준으로 오름차순 정렬
+            List<ConfirmDto> confirmDtoList = reservations.stream()
+                    .filter(reservation -> reservation.getWaitingNumber() != null) // 대기번호가 부여된 예약만 필터링
+                    .map(reservation -> ConfirmDto.builder()
+                            .store(reservation.getStore().getStore()) // 상점 이름
+                            .phone(reservation.getPhone()) // 예약자 번호
+                            .peopleNb(reservation.getPeopleNb()) // 예약 인원
+                            .waitingNumber(reservation.getWaitingNumber()) // 대기번호
+                            .build())
+                    .sorted(Comparator.comparingInt(ConfirmDto::getWaitingNumber)) // 대기번호 순으로 정렬
+                    .collect(Collectors.toList());
+
+            return confirmDtoList;
+        }
 }
